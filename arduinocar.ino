167152093e1b1b74c18ab8aba8cd32005c09ad61
Chenyt <3140100685@zju.edu.cn>
2015-09-08 22:48:37 +0800

    Update arduinocar.ino
    手动模块基本完成，自动部分还不行

--- arduinocar.ino
+++ arduinocar.ino
@@ -1,18 +1,28 @@
 #include <IRremote.h>
 #include <IRremoteInt.h>
 
-int left1 = A0, left2 = A1, right1 = A2, right2 = A3;
-int spd[3];  //three kinds of speed for manu
-int tmp=0;   //speed number
-int nowspd=spd[0];
-int trig = 8, echo = 9;
-int Auto=0;
-static int a[4];
+int left1 = 2, left2 = 3, right1 = 4, right2 = 5;   //四个电机引脚，控制方向用 
+int lastl1, lastl2, lastr1, lastr2;                 //用来保存状态
+int spd[3]={100, 500, 1000};  //设置三种速度
+int tmp=0;                   //当前速度的编号
+int nowspd=spd[0];           //速度初始化为最低速度，手动用
+int spd1=A0, spd2=A1;        //控制两个电机速度：两个变量spd1,spd2
+int trig = 8, echo = 9;      //超声波测距模块
+int Auto=-1;                 //手动/自动,初始化为等待接收信号状态
+static int a[4];             //自动模式下，记录四个方向的距离
+int i, num;
 
-const long manu0=0xFF6897, auto1=0xFF30CF, speedup=0xFFA857, speeddown=0xFFE01F;
-const long go=0XFF18E7, back=0XFF4AB5, trnleft=0xFF10EF, trnright=0xFF5AA5;
+//自动模式：1；手动模式：0，加速：+，减速：-，前进：2，后退：8，左转：4，右转：6.
+const long manu0=0xFF6897,      //遥控器键的值
+           auto1=0xFF30CF, 
+           speedup=0xFFA857, 
+           speeddown=0xFFE01F,
+           go=0XFF18E7, 
+           back=0XFF4AB5, 
+           trnleft=0xFF10EF, 
+           trnright=0xFF5AA5;
 
-int RECV_PIN = 3;
+int RECV_PIN = 6;
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -23,115 +33,183 @@ void setup() {
     pinMode(left2, OUTPUT);
     pinMode(right1, OUTPUT);
     pinMode(right2,OUTPUT);
+    pinMode(spd1, OUTPUT);
+    pinMode(spd2, OUTPUT);
     pinMode(trig, OUTPUT);
     pinMode(echo, INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-    
-    if (irrecv.decode(&results))
+
+    if (irrecv.decode(&results))      //确定&改变模式
     {
+        irrecv.resume();
+        //Serial.println(results.value,HEX);
+
         if (results.value == manu0) Auto = 0;
         else if (results.value == auto1) Auto = 1;
     }
-    irrecv.resume();
-    delay(200);
+    delay(200);   //去扰动
 
-    if (Auto) auto_f();
-    else manual_f();
 
-}
-
-void auto_f()
-{
-    //choose direction
-    //A0，A1:left;  A2,A3:right
-    int i, num;
-    
-    for (i=0;i<4;i++)
+    if (Auto == 1)            //自动避障模式
     {
-    	a[i] = judge_dis();
-    	rotate();
-    }
-
-    num = maxx();
-    for (i=0;i<num;i++) rotate();
-
-    while (true)
-    {
-    	analogWrite(left1,spd[1]);
-    	analogWrite(right1, spd[1]);
-    	int dd = judge_dis();
-    	if (dd <= 30) 
-    	{
-    		analogWrite(left1, 0);
-    		analogWrite(right1, 0);
-    		auto_f();
-    	}
-    }
-
-}
-
-void manual_f()
-{
-    while (true)
-    {
-        if (irrecv.decode(&results))  //press, change or keep state
+        for (i=0;i<4;i++)
         {
-            if (results.value == go) 
+        a[i] = judge_dis();   //测好当前距离之后旋转90度
+        rotate();
+        delay(500);       //停一下（为了看起来不是很蠢
+        }
+
+        num = maxx();             //选出距离最大值
+
+        for (i=0;i<num;i++) rotate();    //旋转到距离最大的方向
+
+        while (true)
+        {
+            analogWrite(spd1,spd[1]);    //自动模式下速度固定
+            analogWrite(spd2, spd[1]);
+
+            digitalWrite(left1, HIGH);   //前进
+            digitalWrite(left2, LOW);
+            digitalWrite(right1, HIGH);
+            digitalWrite(right2, LOW);
+            delay(800);                  //延时，设置每次距离判断间隔时间
+
+            int dd = judge_dis();    
+            if (dd <= 30) 
             {
-                left1 = nowspd;
-                right1 = nowspd;
-                left2 = 0;
-                right2 = 0;
+                digitalWrite(left1, LOW);
+                digitalWrite(right1, LOW);
+                break;
             }
-            else if (results.value == back)
+        }
+        
+
+    }
+    else if (Auto == 0)
+    {
+        //Serial.println("in");
+        if (irrecv.decode(&results))  //检测到按键，改变或保持当前状态
+        {
+            //Serial.println("resd");
+            //Serial.println("manual_true");
+            if (results.value == go)               //前进
             {
-                left1 = 0;
-                right1 = 0;
-                left2 = nowspd;
-                right2 = nowspd;
+                analogWrite(spd1, nowspd);         //设置速度
+                analogWrite(spd2, nowspd);
+
+                digitalWrite(left1, HIGH);
+                digitalWrite(left2, LOW);
+                digitalWrite(right1, HIGH);
+                digitalWrite(right2, LOW);
+
+                delay(100);              //当前状态延时
+
+                lastl1 = 1;              //保存状态
+                lastl2 = 0;
+                lastr1 = 1;
+                lastr2 = 0;
+
             }
-            else if (results.value == trnleft)
+            else if (results.value == back)    //后退
             {
-                left1 = 0;
-                right1 = spd[0];
-                left2 = 0;
-                right2 = 0;
+                analogWrite(spd1, nowspd);         //设置速度
+                analogWrite(spd2, nowspd);
+
+                digitalWrite(left1, LOW);
+                digitalWrite(left2, HIGH);
+                digitalWrite(right1, LOW);
+                digitalWrite(right2, HIGH);
+
+                delay(100);
+                
+                lastl1 = 0;              //保存状态
+                lastl2 = 1;
+                lastr1 = 0;
+                lastr2 = 1;
+
             }
-            else if (results.value == trnright)
+            else if (results.value == trnleft)     //左转
             {
-                left1 = spd[0];
-                right1 = 0;
-                left2 = 0;
-                right2 = 0;
+                analogWrite(spd1, nowspd);         //设置速度
+                analogWrite(spd2, nowspd);
+
+                digitalWrite(left1, LOW);
+                digitalWrite(left2, HIGH);
+                digitalWrite(right1, HIGH);
+                digitalWrite(right2, LOW);
+
+                delay(100);
+                
+                lastl1 = 0;
+                lastl2 = 1;
+                lastr1 = 1;
+                lastr2 = 0;
+
             }
-            else if (results.value == speedup)
+            else if (results.value == trnright)    //右转
+            {
+                analogWrite(spd1, nowspd);         //设置速度
+                analogWrite(spd2, nowspd);
+
+                digitalWrite(left1, HIGH);
+                digitalWrite(left2, LOW);
+                digitalWrite(right1, LOW);
+                digitalWrite(right2, HIGH);
+
+                delay(100);
+                
+                lastl1 = 1;
+                lastl2 = 0;
+                lastr1 = 0;
+                lastr2 = 1;
+
+            }
+            else if (results.value == speedup)     //加速，改变速度只能短按不能长按
             {
                 tmp++;
-                if (tmp >= 3) tmp = 2;
+                if (tmp >= 3) tmp = 2;             //设置边界
+                nowspd = spd[tmp];
             }
-            else if (results.value == speeddown)
+            else if (results.value == speeddown)     //减速
             {
                 tmp--;
-                if (tmp < 0) tmp = 0;
+                if (tmp < 0) tmp = 0;              //边界
+                nowspd = spd[tmp];
+            }
+            else                                  //如果是0xFFFFFFFF,延续上次状态
+            {
+                analogWrite(spd1, nowspd);         //设置速度
+                analogWrite(spd2, nowspd);
+
+                digitalWrite(left1, lastl1);
+                digitalWrite(left2, lastl2);
+                digitalWrite(right1, lastr1);
+                digitalWrite(right2, lastr2);
+
+                delay(100);
             }
 
         }
-        else                               //else, stop
+        else
         {
-            left1 = 0;
-            right1 = 0;
-            left2 = 0;
-            right2 = 0;
+            digitalWrite(left1, LOW);
+            digitalWrite(left2, LOW);
+            digitalWrite(right1, LOW);
+            digitalWrite(right2, LOW);
         }
-        irrecv.resume();
+        
+    
     }
+
+
 }
 
 int judge_dis()
 {
-    //super
+    //超声波测距
     //trig:8, echo:9
 
     digitalWrite(trig, LOW);
@@ -146,24 +224,32 @@ int judge_dis()
 
 void rotate()
 {
-	int once; //time of rotating once
-	analogWrite(left1, spd[1]);
-	analogWrite(left2, 0);
-	analogWrite(right1, 0);
-	analogWrite(right2, spd[1]);
-	delay(once);
+	int once=750;                //旋转90度的时间
 
-	analogWrite(left1, 0);
-	analogWrite(right2, 0);
+    spd1 = spd[1];               //旋转使用速度spd[1]
+    spd2 = spd[1];
+
+    digitalWrite(left1, LOW);
+    digitalWrite(left2, HIGH);
+    digitalWrite(right1, HIGH);
+    digitalWrite(right2, LOW);
+    delay(once);
+
+    digitalWrite(left2, LOW);
+    digitalWrite(right1, LOW);
 
 }
 
 int maxx()
 {
 	int m = a[0];
-	int i;
+	int i, t=0;
 	for (i=1;i<=3;i++)
-	    if (a[i] > m) m = a[i];
+    if (a[i] > m)
+    {
+        m = a[i];
+        t = i;
+    }
 
-	return m;
+   return t;
 }
