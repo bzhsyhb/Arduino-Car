e35ee9619f105ab2493370908a3b54a468aca899
Chenyt <3140100685@zju.edu.cn>
2015-09-09 23:19:34 +0800

    增加了前进防撞功能，自动部分还需测试

--- arduinocar.ino
+++ arduinocar.ino
@@ -3,14 +3,15 @@
 
 int left1 = 2, left2 = 3, right1 = 4, right2 = 5;   //四个电机引脚，控制方向用 
 int lastl1, lastl2, lastr1, lastr2;                 //用来保存状态
-int spd[3]={100, 500, 1000};  //设置三种速度
-int tmp=0;                   //当前速度的编号
-int nowspd=spd[0];           //速度初始化为最低速度，手动用
-int spd1=A0, spd2=A1;        //控制两个电机速度：两个变量spd1,spd2
-int trig = 8, echo = 9;      //超声波测距模块
-int Auto=-1;                 //手动/自动,初始化为等待接收信号状态
-static int a[4];             //自动模式下，记录四个方向的距离
+int spd[3]={200, 500, 1000};  //设置三种速度
+int tmp=0;                    //当前速度的编号
+int nowspd=spd[0];            //速度初始化为最低速度，手动用
+int spd1=A0, spd2=A1;         //控制两个电机速度：两个变量spd1,spd2
+int trig = 8, echo = 9;       //超声波测距模块
+int Auto=-1;                  //手动/自动,初始化为等待接收信号状态
+static int a[4];              //自动模式下，记录四个方向的距离
 int i, num;
+int led = 10; 
 
 //自动模式：1；手动模式：0，加速：+，减速：-，前进：2，后退：8，左转：4，右转：6.
 const long manu0=0xFF6897,      //遥控器键的值
@@ -37,6 +38,7 @@ void setup() {
     pinMode(spd2, OUTPUT);
     pinMode(trig, OUTPUT);
     pinMode(echo, INPUT);
+    pinMode(led, OUTPUT);
     Serial.begin(9600);
 }
 
@@ -53,18 +55,21 @@ void loop() {
     delay(200);   //去扰动
 
 
-    if (Auto == 1)            //自动避障模式（有错误）
+    if (Auto == 1)            //自动避障模式
     {
         for (i=0;i<4;i++)
         {
         a[i] = judge_dis();   //测好当前距离之后旋转90度
+        Serial.println(a[i]);
         rotate();
         delay(500);       //停一下（为了看起来不是很蠢
         }
 
-        num = maxx();             //选出距离最大值
+        num = maxx();             //选出距离最大值的编号
 
         for (i=0;i<num;i++) rotate();    //旋转到距离最大的方向
+
+        Serial.println(num);
 
         while (true)
         {
@@ -91,9 +96,10 @@ void loop() {
     else if (Auto == 0)
     {
         //Serial.println("in");
+
         if (irrecv.decode(&results))  //检测到按键，改变或保持当前状态
         {
-            //Serial.println("resd");
+            //Serial.println("read");
             //Serial.println("manual_true");
             if (results.value == go)               //前进
             {
@@ -105,16 +111,26 @@ void loop() {
                 digitalWrite(right1, HIGH);
                 digitalWrite(right2, LOW);
 
-                delay(100);              //当前状态延时
-
                 lastl1 = 1;              //保存状态
                 lastl2 = 0;
                 lastr1 = 1;
                 lastr2 = 0;
 
+                if (judge_dis() <= 4)
+                {
+                    digitalWrite(left1, LOW);
+                    digitalWrite(left2, LOW);
+                    digitalWrite(right1, LOW);
+                    digitalWrite(right2, LOW);
+
+                    digitalWrite(led, HIGH);
+                }
+
             }
             else if (results.value == back)    //后退
             {
+                digitalWrite(led, LOW);
+
                 analogWrite(spd1, nowspd);         //设置速度
                 analogWrite(spd2, nowspd);
 
@@ -122,8 +138,6 @@ void loop() {
                 digitalWrite(left2, HIGH);
                 digitalWrite(right1, LOW);
                 digitalWrite(right2, HIGH);
-
-                delay(100);
                 
                 lastl1 = 0;              //保存状态
                 lastl2 = 1;
@@ -133,6 +147,8 @@ void loop() {
             }
             else if (results.value == trnleft)     //左转
             {
+                digitalWrite(led, LOW);
+
                 analogWrite(spd1, nowspd);         //设置速度
                 analogWrite(spd2, nowspd);
 
@@ -140,8 +156,6 @@ void loop() {
                 digitalWrite(left2, HIGH);
                 digitalWrite(right1, HIGH);
                 digitalWrite(right2, LOW);
-
-                delay(100);
                 
                 lastl1 = 0;
                 lastl2 = 1;
@@ -151,6 +165,8 @@ void loop() {
             }
             else if (results.value == trnright)    //右转
             {
+                digitalWrite(led, LOW);
+
                 analogWrite(spd1, nowspd);         //设置速度
                 analogWrite(spd2, nowspd);
 
@@ -158,8 +174,6 @@ void loop() {
                 digitalWrite(left2, LOW);
                 digitalWrite(right1, LOW);
                 digitalWrite(right2, HIGH);
-
-                delay(100);
                 
                 lastl1 = 1;
                 lastl2 = 0;
@@ -189,9 +203,18 @@ void loop() {
                 digitalWrite(right1, lastr1);
                 digitalWrite(right2, lastr2);
 
-                delay(100);
+                if (judge_dis() <= 4 && judgego(lastl1,lastl2,lastr1,lastr2))
+                {
+                    digitalWrite(left1, LOW);
+                    digitalWrite(left2, LOW);
+                    digitalWrite(right1, LOW);
+                    digitalWrite(right2, LOW);
+
+                    digitalWrite(led, HIGH);
+                }
             }
 
+            delay(100);                             //当前状态延时
         }
         else
         {
@@ -205,6 +228,12 @@ void loop() {
     }
 
 
+}
+
+int judgego(int s1, int s2, int s3, int s4)
+{
+    if (s1 == 1 && s2 == 0 && s3 == 1 && s4 == 0)
+        return 1;
 }
 
 int judge_dis()
@@ -226,8 +255,8 @@ void rotate()
 {
 	int once=750;                //旋转90度的时间
 
-    spd1 = spd[1];               //旋转使用速度spd[1]
-    spd2 = spd[1];
+    analogWrite(spd1,spd[1]);               //旋转使用速度spd[1]
+    analogWrite(spd2, spd[1]);
 
     digitalWrite(left1, LOW);
     digitalWrite(left2, HIGH);
@@ -237,6 +266,7 @@ void rotate()
 
     digitalWrite(left2, LOW);
     digitalWrite(right1, LOW);
+
 }
 
 int maxx()
@@ -244,11 +274,11 @@ int maxx()
 	int m = a[0];
 	int i, t=0;
 	for (i=1;i<=3;i++)
-    if (a[i] > m)
-    {
-        m = a[i];
-        t = i;
-    }
+        if (a[i] > m)
+        {
+            m = a[i];
+            t = i;
+        }
 
    return t;
 }
